c67425cc99031998fdc521b2769a5063e56c6615
Christina Chianelli <cechianelli@gmail.com>
2020-05-21 20:46:41 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -1,8 +1,8 @@
-def using_push (array, string)
+def using_push (array,string)
   array.push(string)
 end
 
-def using_unshift (array, string)
+def using_unshift (array,string)
   array.unshift(string)
 end
 
